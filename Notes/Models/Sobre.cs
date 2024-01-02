c7b9002d39c8f98254d7b762fae6e07e8b660580
kauan <kauan@ROBERTA>
2024-01-02 13:30:45 -0300

    Adicionar arquivos de projeto.

---
+++ Notes/Models/Sobre.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://learn.microsoft.com/pt-br/dotnet/maui/tutorials/notes-app/?view=net-maui-8.0&tutorial-step=4";
+    public string Message => "Esse aplicativo foi feito com base nos ensinamentos contidos em:";
+}
